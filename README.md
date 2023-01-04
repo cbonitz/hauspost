66ac6f9bc604de562d78eb392818d7f3ac7482fd
Christoph Bonitz <ml.christophbonitz@gmail.com>
2023-01-04 13:05:37 +0100

    license, copyright notice

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple In-Process Message Exchange
+
+This is a simple topic-based in-process message exchange for use in [Tokio](https://tokio.rs/) applications.
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! Simple in-memory message exchange
+//! Simple topic-based in-process message exchange for use in [Tokio](https://tokio.rs/) applications.
 pub mod exchange;
 pub mod queue;
 pub mod requests;
